7ceff58e0658fda4212f789d0394fb2519d4b7ff
octavio mejia <arkord1@gmail.com>
2024-05-29 02:18:40 -0500

    inputs

--- app/(tabs)/other.tsx
+++ app/(tabs)/other.tsx
@@ -1,19 +1,44 @@
-import { View, StyleSheet, Text } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, Text, TextInput, Button, Alert } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaProvider, useSafeAreaInsets  } from 'react-native-safe-area-context';
+import { Link } from 'expo-router';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
 
 export default function Other() {
   const insets = useSafeAreaInsets();
+  const [text, onChangeText] = useState('Useless Text');
+  const [number, onChangeNumber] = useState('');
+
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider style={{ backgroundColor: "#000000"}}>
     <View style={{ flex: 1, paddingTop: insets.top }}>
           <Text style={styles.parrafo}>Other component</Text>
       </View>
+
+      <View>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+      <Button
+          title="Right button"
+          onPress={() => alert(text)}
+        />
+      </View>
+
+      <Link href="/">Go Home</Link>
     </SafeAreaProvider>
     
   )
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -23,5 +48,13 @@ const styles = StyleSheet.create({
     color: 'red',
     fontWeight: 'bold',
     fontSize: 30
-  }
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    color: "#ffffff",
+    borderColor: "#ffffff",
+    padding: 10,
+  },
 });
